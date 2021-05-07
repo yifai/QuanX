534daba03268d22eb7e9018981a7f8a021961cec
Ming <70466109+yeefaye@users.noreply.github.com>
2021-05-07 16:30:11 +0800

    Update ip_1.js

--- ip_1.js
+++ ip_1.js
@@ -4,7 +4,7 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = obj['city'] + ':' obj['country']
+var title = obj['city']
 var subtitle = obj['country'] + ' ' + obj['isp'];
 var ip = obj['query'];
 var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
