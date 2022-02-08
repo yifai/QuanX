4f503c1ddd7b2cf0ffa852c3db3cced56535d9c2
Ming <70466109+yeefaye@users.noreply.github.com>
2022-02-08 18:50:39 +0800

    Update IP_1.js

--- IP_1.js
+++ IP_1.js
@@ -36,5 +36,5 @@ var obj = JSON.parse(body);
 var title =flags.get(obj['countryCode']) + ' '+ City_ValidCheck(obj['city']);//+Area_check(obj['country']);
 var subtitle = ISP_ValidCheck(obj['org']);
 var ip = obj['query'];
-var description = '服务商:'+obj['isp'] + '\n' + '地区' + ":" + City_ValidCheck(obj['city']) + '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'] + '\n' + 'IP:'+ obj['query'];
+var description = '服务商:'+obj['isp'] + '\n' + '地区' + ":" + City_ValidCheck(obj['city']) + '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
